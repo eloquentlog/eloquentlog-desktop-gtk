247284af597a2bdc63caf8e4de3f85169d2169a7
Yasuhiro Яша Asaka <yasuhiro.asaka@grauwoelfchen.net>
2021-05-25 04:46:56 +0900

    Add XML sources

--- src/bin/eloquentlog.rs
+++ src/bin/eloquentlog.rs
@@ -7,37 +7,21 @@ use gio::prelude::*;
 use gtk::prelude::*;
 
 use gtk::{
-    AboutDialog, AccelGroup, Application, ApplicationWindow, Label, Menu,
-    MenuBar, MenuItem, Orientation, WindowPosition, StackBuilder,
-    StackSidebarBuilder,
+    AboutDialog, AccelGroup, Application, ApplicationWindow, Builder, Label,
+    Menu, MenuBar, MenuItem, Orientation, StackBuilder, StackSidebarBuilder,
 };
 
-const WEBSITE_URL: &str =
-    "https://gitlab.com/eloquentlog/eloquentlog-desktop-gtk";
-
 fn build_ui(app: &Application) {
-    let win = ApplicationWindow::new(app);
-
-    win.set_title("Eloquentlog");
-    win.set_border_width(10);
-    win.set_position(WindowPosition::Center);
-
-    let vbox = gtk::Box::new(Orientation::Vertical, 10);
+    let awin_src = include_str!("../ui/window.xml");
+    let awin: ApplicationWindow = Builder::from_string(awin_src)
+        .get_object("window")
+        .expect("couldn't get window");
+    awin.set_application(Some(app));
 
     let accl_grp = AccelGroup::new();
-    win.add_accel_group(&accl_grp);
+    awin.add_accel_group(&accl_grp);
 
-    // menubar
-    let bar = MenuBar::new();
-
-    // sidebar
-    let stack = StackBuilder::new().expand(true).visible(true).build();
-    let pref_lbl = Label::new(Some("Preferences"));
-    stack.add_titled(&pref_lbl, "preferences", "Preferences");
-    let sidebar = StackSidebarBuilder::default().build();
-    sidebar.set_stack(&stack);
-
-    // file
+    // menu
     let file = MenuItem::with_label("File");
     let about = MenuItem::with_label("About");
     let quit = MenuItem::with_label("Quit");
@@ -47,9 +31,33 @@ fn build_ui(app: &Application) {
     menu.append(&quit);
     file.set_submenu(Some(&menu));
 
-    bar.append(&file);
+    // menubar
+    let mbar_src = include_str!("../ui/menubar.xml");
+    let mbar: MenuBar = Builder::from_string(mbar_src)
+        .get_object("menubar")
+        .expect("couldn't get menubar");
+    mbar.append(&file);
 
-    let weak = win.downgrade();
+    // sidebar
+    let stack = StackBuilder::new().expand(true).visible(true).build();
+    let pref_lbl = Label::new(Some("Preferences"));
+    stack.add_titled(&pref_lbl, "preferences", "Preferences");
+    let sbar = StackSidebarBuilder::default().build();
+    sbar.set_stack(&stack);
+
+    // main
+    let lbl = Label::new(Some("Sample"));
+
+    // https://gtk-rs.org/docs/gtk/trait.BoxExt.html#tymethod.pack_start
+    let vbox = gtk::Box::new(Orientation::Vertical, 10);
+    vbox.pack_start(&mbar, false, true, 0);
+    vbox.pack_start(&sbar, false, true, 0);
+    vbox.pack_start(&lbl, true, true, 0);
+    awin.add(&vbox);
+
+    awin.show_all();
+
+    let weak = awin.downgrade();
     quit.connect_activate(move |_| {
         let win = match weak.upgrade() {
             Some(o) => o,
@@ -58,32 +66,21 @@ fn build_ui(app: &Application) {
         win.close();
     });
 
-    // main
-    let lbl = Label::new(Some("Sample"));
-    // https://gtk-rs.org/docs/gtk/trait.BoxExt.html#tymethod.pack_start
-    vbox.pack_start(&bar, false, true, 0);
-    vbox.pack_start(&sidebar, false, true, 0);
-    vbox.pack_start(&lbl, true, true, 0);
-    win.add(&vbox);
-
-    win.show_all();
-
     about.connect_activate(move |_| {
-        let dlg = AboutDialog::new();
-        dlg.set_title("About");
-        dlg.set_website_label(Some("Eloquentlog Desktop for Linux"));
-        dlg.set_website(Some(WEBSITE_URL));
-        dlg.set_authors(&["Lupine Software LLC"]);
-        dlg.set_transient_for(Some(&win));
-        dlg.run();
-        dlg.close();
+        let dlog_src = include_str!("../ui/about_dialog.xml");
+        let dlog: AboutDialog = Builder::from_string(dlog_src)
+            .get_object("about_dialog")
+            .expect("couldn't get about_dialog");
+        dlog.set_transient_for(Some(&awin));
+        dlog.run();
+        dlog.close();
     });
 }
 
 fn main() {
     let application =
         Application::new(Some("com.eloquentlog.desktop"), Default::default())
-            .expect("Initialization faild");
+            .expect("couldn't get initialized");
     application.connect_activate(|app| {
         build_ui(app);
     });
